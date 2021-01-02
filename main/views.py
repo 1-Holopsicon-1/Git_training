0126c93082fe45f84c66f7775678c3e36b5d4c2b
LuparevSergey <sluparev@yandex.ru>
2021-01-02 13:39:17 +0300

    fixed some stupid mistakes

--- main/views.py
+++ main/views.py
@@ -6,6 +6,6 @@ from accounts.models import UserProperties
 def index(request):
     context = {}
     if request.user.is_authenticated:
-        context['userProperties'] = UserProperties.objects.get(user=request.user)
-    context['is_staff']: request.user.is_staff
+        context['userProperties']: UserProperties.objects.get(user=request.user)
+        context['is_staff']: request.user.is_staff
     return render(request, 'index.html', context)
